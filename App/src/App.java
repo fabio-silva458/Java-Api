10e6c4efa51d2676547555bdc1dc5199a8451e82
fabio-silva458 <fabiomyssionhills@gmail.com>
2022-07-26 21:10:39 -0300

    Criando conexão com a API

--- App/src/App.java
+++ App/src/App.java
@@ -1,5 +1,17 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        //Tarefa: Criar uma conexão HTTP e buscar o Top 10 filmes.
+		String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java/api/TopMovies.json";
+		URI endereco = URI.create(url);
+		HttpClient cliente = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
+		HttpResponse<String> response = cliente.send(request, BodyHandlers.ofString());
+		String body = response.body();
+}
 }
